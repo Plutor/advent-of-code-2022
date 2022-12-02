f8da83b9cbf9d30af8310189630c872abc8cb7b6
Logan Ingalls <lingalls@gmail.com>
2022-12-02 09:35:09 -0500

    Day 2

---
+++ 2.hs
@@ -0,0 +1,16 @@
+import Data.Char (ord)
+import Data.Set (member, fromList)
+
+shapeScore :: [Char] -> Int
+shapeScore m = ord (last m) - ord 'W'
+
+outcomeScore :: [Char] -> Int
+outcomeScore m = if member m (fromList ["A X", "B Y", "C Z"]) then 3 -- tie
+  else if member m (fromList ["A Y", "B Z", "C X"]) then 6 -- win
+  else 0
+
+main = do
+  d <- readFile "data/2.txt"
+  let l = lines d
+  let sc = foldl (\acc m -> acc + shapeScore m + outcomeScore m) 0 l
+  print sc
---
+++ 2b.hs
@@ -0,0 +1,16 @@
+import Data.Char (ord)
+import Data.Set (member, fromList)
+
+shapeScore :: [Char] -> Int
+shapeScore m = if member m (fromList ["A Y", "B X", "C Z"]) then 1 -- rock
+  else if member m (fromList ["A Z", "B Y", "C X"]) then 2 -- paper
+  else 3  -- scissors
+
+outcomeScore :: [Char] -> Int
+outcomeScore m = (ord (last m) - ord 'X') * 3
+
+main = do
+  d <- readFile "data/2.txt"
+  let l = lines d
+  let sc = foldl (\acc m -> acc + shapeScore m + outcomeScore m) 0 l
+  print sc
