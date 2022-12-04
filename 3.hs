801d0959a1896a73d1997c738951e0ec019447be
Logan Ingalls <lingalls@gmail.com>
2022-12-03 20:43:37 -0500

    Day 3 part 1

---
+++ 3.hs
@@ -0,0 +1,19 @@
+import Data.Char (ord)
+import Data.Set (fromList, intersection, elemAt)
+
+rucksackHalves :: [Char] -> ([Char], [Char])
+rucksackHalves sack = (
+  take (div (length sack) 2) sack,
+  drop (div (length sack) 2) sack)
+
+commonItem :: [Char] -> [Char] -> Char
+commonItem a b = elemAt 0 (intersection (fromList a) (fromList b))
+
+itemVal :: Char -> Int
+itemVal a = if a <= 'Z' then 27 + ord(a) - ord('A') else 1 + ord(a) - ord('a')
+
+main = do
+  d <- readFile "data/3.txt"
+  let l = lines d
+  let tot = foldl (\acc r -> acc + (itemVal . uncurry commonItem . rucksackHalves $ r)) 0 l
+  print tot
