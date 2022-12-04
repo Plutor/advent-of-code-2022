8c4daa08bdaf03712fb684ff33b2a6b63a5c4fe9
Logan Ingalls <lingalls@gmail.com>
2022-12-03 20:58:20 -0500

    Day 3 part 2

---
+++ 3b.hs
@@ -0,0 +1,17 @@
+import Data.Char (ord)
+import Data.List.Split  -- cabal install --lib split
+import Data.Set (fromList, intersection, elemAt)
+
+commonItem :: [[Char]] -> Char
+commonItem abc = elemAt 0 (intersection (intersection
+  (fromList $ abc !! 0) (fromList $ abc !! 1)) (fromList $ abc !! 2))
+
+itemVal :: Char -> Int
+itemVal a = if a <= 'Z' then 27 + ord(a) - ord('A') else 1 + ord(a) - ord('a')
+
+main = do
+  d <- readFile "data/3.txt"
+  let l = chunksOf 3 . lines $ d
+  print l
+  let tot = foldl (\acc r -> acc + (itemVal . commonItem $ r)) 0 l
+  print tot
