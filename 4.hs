77659aae376f8ff105ae9e67589584bd58a19e96
Logan Ingalls <lingalls@gmail.com>
2022-12-04 21:01:39 -0500

    Day 4

---
+++ 4.hs
@@ -0,0 +1,14 @@
+import Data.List.Split (splitOneOf)
+
+readInt :: String -> Int
+readInt = read
+
+parseData :: [String] -> [[Int]]
+parseData = map (\e -> map readInt (splitOneOf ",-" e))
+
+main = do
+  d <- readFile "data/4.txt"
+  let l = parseData . lines $ d
+  print l
+  let c = length (filter (\v -> (v!!0 <= v!!2 && v!!1 >= v!!3) || (v!!0 >= v!!2 && v!!1 <= v!!3)) l)
+  print c
---
+++ 4b.hs
@@ -0,0 +1,13 @@
+import Data.List.Split (splitOneOf)
+
+readInt :: String -> Int
+readInt = read
+
+parseData :: [String] -> [[Int]]
+parseData = map (\e -> map readInt (splitOneOf ",-" e))
+
+main = do
+  d <- readFile "data/4.txt"
+  let l = parseData . lines $ d
+  let c = length (filter (\v -> not (v!!0 < v!!2 && v!!0 < v!!3 && v!!1 < v!!2 && v!!1 < v!!3) && not (v!!0 > v!!2 && v!!0 > v!!3 && v!!1 > v!!2 && v!!1 > v!!3)) l)
+  print c
