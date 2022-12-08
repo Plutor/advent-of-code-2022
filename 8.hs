39b9f814d8f1b8961ed8f649410146162b4c778c
Logan Ingalls <lingalls@gmail.com>
2022-12-08 11:12:06 -0500

    Day 8 part 1 closer...

--- 8.hs
+++ 8.hs
@@ -1,24 +1,19 @@
-readInt :: String -> Int
-readInt = read
+rotate :: [a] -> [a]
+rotate f = foldl (\out i -> out ++ [f !! ((mod i 99)*99 + (div i 99))]) [] [0..9801]
 
-rotate :: String -> String
-rotate f = foldl (\out i -> out ++ [f !! ((mod i 4)*4 + (div i 4))]) "" [0..]
-
-visibleInRow :: String -> Int -> Int -> Bool
-visibleInRow f _ 0 = True
-visibleInRow f _ 98 = True
-visibleInRow f 0 _ = True
-visibleInRow f 98 _ = True
-visibleInRow f x y = fst (foldl
-  (\(v,mx) (tx,h) -> if v                    then (v,mx)
-                     else if tx==x && h > mx then (True,mx)
-                     else if tx<x  && h > mx then (v,h)
-                     else if tx>x  && h > mx then (False,mx)
-                     else (v,mx))
-  (False, '/')
-  (zip [0..] (take 99 (drop (99*y) f))))
+markVisiblesFromLeft :: String -> [Bool] -> [Bool]
+markVisiblesFromLeft f v = fst (foldl
+  (\(vl,mx) (i,h) -> if (mod i 99) == 0 || h > mx then (vl ++ [True], h)
+                     else (vl ++ [v !! i], mx))
+  ([], '/')
+  (zip [0..] f))
 
 main = do
   d <- readFile "data/8.txt"
   let f = concat . lines $ d
-  print . sum $ (map (\i -> if visibleInRow f (mod i 99) (div i 99) || visibleInRow (rotate f) (mod i 99) (div i 99) then 1 else 0) [0..9801])
+  let v = snd (foldl (\(fo,v) _ -> (rotate fo, rotate ( markVisiblesFromLeft fo v )))
+                     (f, replicate (length f) False)
+                     [0..3])
+  print . length $ (filter (==True) v)
+
+-- 1059 is too low
